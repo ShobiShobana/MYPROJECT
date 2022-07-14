55f324d1a81a48214872fd52ea326bfb186be6ba
ShobiShobana <109290219+ShobiShobana@users.noreply.github.com>
2022-07-14 18:24:13 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dropdown from './select'
+// import Dropdownnew from './newform';
+
+function App() {
+  return (
+    <div className="App">
+      <Dropdown/>
+    </div>
+  );
+}
+
+export default App;
